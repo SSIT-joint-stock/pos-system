be6b7ee18bf32a88baaff887e926e7b28c3f19c8
Bùi An Du <vuduycp123@gmail.com>
2025-08-08 21:29:54 +0700

    feat(page): add logo image to main page - add something magic - ok

--- apps/web/main/src/app/page.tsx
+++ apps/web/main/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Heading, Text } from "@repo/design-system/components/ui/typography";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
+        <Image src="/logo.png" alt="Logo" width={100} height={100} />
         <Heading level={3}>Hello World</Heading>
         <Text c="dimmed">Welcome to the main domain</Text>
       </div>
