82c16c1440c97fb5e15a192cd3c6b2a0a9d5495b
trh-thanh30 <tranhuuthanhcp@gmail.com>
2025-08-20 13:46:55 +0700

    feat(manage-view): enhance product management UI

--- packages/design-system/src/components/layout/dashboard-layout.tsx
+++ packages/design-system/src/components/layout/dashboard-layout.tsx
@@ -16,11 +16,9 @@ export default function DashboardLayout({
       </aside>
 
       <div className="flex-1 flex flex-col">
-        <header className="bg-white z-10 border border-gray-100 p-4">
-          <header>LOGO</header>
-        </header>
-
-        <main className="flex-1 p-4 overflow-y-auto bg-gray-50  scrollbar-thin scrollbar-thumb-gray-50 scrollbar-track-transparent">{children}</main>
+        <main className="flex-1 p-4 overflow-y-auto bg-gray-50  scrollbar-thin scrollbar-thumb-gray-50 scrollbar-track-transparent">
+          {children}
+        </main>
       </div>
     </div>
   );
