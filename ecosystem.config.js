3e76ccfbf31e37a54a95643b1b9c3b6a694ea815
duyvu871 <vuduycp123@gmail.com>
2025-09-23 19:55:16 +0700

    feat(config): update start script ports and add ecosystem configuration for apps

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+import { join } from 'path';
+const cwd = process.cwd();
+
+export const apps = [
+    {
+        name: 'pos-web-main',
+        script: 'npm run start',
+        cwd: join(cwd, 'apps/web/main'),
+        watch: false,
+        env_production: {
+            NODE_ENV: 'production',
+        },
+        out_file: "./front-end-out.log",
+        error_file: "./front-end-error.log",
+        log_date_format: "DD-MM HH:mm:ss Z",
+    },
+    {
+        name: 'pos-web-retail',
+        script: 'npm run start',
+        cwd: join(cwd, 'apps/web/retail'),
+        watch: false,
+        env_production: {
+            NODE_ENV: 'production',
+        },
+        out_file: "./front-end-out.log",
+        error_file: "./front-end-error.log",
+        log_date_format: "DD-MM HH:mm:ss Z",
+    }
+];
