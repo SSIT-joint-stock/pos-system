678f430a7ea6fd12bee3a1aef6eca5dc58179fc1
trh-thanh30 <tranhuuthanhcp@gmail.com>
2025-09-29 17:10:17 +0700

    refactor: standardize string quotes and improve formatting in filter-bar and manage-stock-view components

--- apps/web/main/src/sections/dashboard/components/filter-bar.tsx
+++ apps/web/main/src/sections/dashboard/components/filter-bar.tsx
@@ -1,7 +1,7 @@
-"use client";
-import { AutoComplete, DatePickerInput, Select } from "@repo/design-system/components/ui";
-import { Calendar1, Search } from "lucide-react";
-import * as React from "react";
+'use client';
+import { AutoComplete, DatePickerInput, Select } from '@repo/design-system/components/ui';
+import { Calendar1, Search } from 'lucide-react';
+import * as React from 'react';
 
 type FilterOption = {
   label: string;
@@ -12,10 +12,15 @@ type FilterBarProps = {
   onSearch?: (value: string) => void;
   statusOptions?: FilterOption[];
   categoryOptions?: FilterOption[];
-  onFilterChange?: (filters: { status?: string; category?: string; date?: [string, string] }) => void;
+  onFilterChange?: (filters: {
+    status?: string;
+    category?: string;
+    date?: [string, string];
+  }) => void;
   actions?: React.ReactNode;
   hasBg?: boolean;
   setWidth?: string;
+  hasDatePicker?: boolean;
 };
 
 export default function FilterBar({
@@ -25,12 +30,13 @@ export default function FilterBar({
   onFilterChange,
   actions,
   hasBg = true,
-  setWidth = "34%",
+  setWidth = '34%',
+  hasDatePicker = true,
 }: FilterBarProps) {
   const [status, setStatus] = React.useState<string | undefined>(undefined);
   const [category, setCategory] = React.useState<string | undefined>(undefined);
   const [date, setDate] = React.useState<string[]>([]);
-  const [searchValue, setSearchValue] = React.useState<string>("");
+  const [searchValue, setSearchValue] = React.useState<string>('');
 
   // Ref to track if component has mounted
   const hasMounted = React.useRef(false);
@@ -54,7 +60,7 @@ export default function FilterBar({
     onFilterChange?.({
       status,
       category,
-      date: [date[0]?.toString() ?? "", date[1]?.toString() ?? ""] as [string, string],
+      date: [date[0]?.toString() ?? '', date[1]?.toString() ?? ''] as [string, string],
     });
   }, [status, category, date]);
   // Handle search when button is clicked
@@ -68,7 +74,7 @@ export default function FilterBar({
   };
 
   return (
-    <div className={`flex items-center ${hasBg ? "bg-white p-5 rounded-lg shadow" : ""}`}>
+    <div className={`flex items-center ${hasBg ? 'bg-white p-5 rounded-lg shadow' : ''}`}>
       <div className="flex items-center w-full gap-2">
         {/* SEARCH */}
         <form
@@ -85,10 +91,14 @@ export default function FilterBar({
             leftSection={<Search size={16} />}
             variant="unstyled"
             placeholder="Tìm kiếm sản phẩm"
-            data={["T-Shirt", "Cap", "Shoes", "Watch", "Sunglass"]}
+            data={['T-Shirt', 'Cap', 'Shoes', 'Watch', 'Sunglass']}
             className="flex-1 py-[1px] text-sm text-gray-900 font-medium placeholder:font-normal"
             rightSection={
-              <button onClick={handleSearchClick} className="bg-pos-blue-500 text-white p-2" title="Tìm kiếm">
+              <button
+                onClick={handleSearchClick}
+                className="bg-pos-blue-500 text-white p-2"
+                title="Tìm kiếm"
+              >
                 <Search size={16} />
               </button>
             }
@@ -123,20 +133,22 @@ export default function FilterBar({
             />
           )}
 
-          <div className="w-[26ch] border border-gray-200 rounded-md outline-none">
-            <DatePickerInput
-              type="range"
-              variant="unstyled"
-              radius="md"
-              clearable
-              placeholder="VD: 15/08/2025-22/08/2025"
-              size="xs"
-              value={date}
-              onChange={(val) => setDate((val as string[]) || [])}
-              rightSection={<Calendar1 size={16} />}
-              className="w-full text-nowrap py-[1px] px-2 text-sm text-gray-900 font-medium placeholder:font-normal placeholder:text-gray-900"
-            />
-          </div>
+          {hasDatePicker && (
+            <div className="w-[26ch] border border-gray-200 rounded-md outline-none">
+              <DatePickerInput
+                type="range"
+                variant="unstyled"
+                radius="md"
+                clearable
+                placeholder="VD: 15/08/2025-22/08/2025"
+                size="xs"
+                value={date}
+                onChange={(val) => setDate((val as string[]) || [])}
+                rightSection={<Calendar1 size={16} />}
+                className="w-full text-nowrap py-[1px] px-2 text-sm text-gray-900 font-medium placeholder:font-normal placeholder:text-gray-900"
+              />
+            </div>
+          )}
         </div>
       </div>
 
